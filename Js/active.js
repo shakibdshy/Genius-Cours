b10777c8f0e0172dae5b2d4591b6dad70de1620e
shakibdshy <shakibdshy@gmail.com>
2018-09-20 18:40:55 +0600

    first upload

---
+++ Js/active.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+      // Slider Area
+      $('.slider-active').owlCarousel({
+            loop: true,
+            nav: true,
+            navText: ['<i class="fal fa-chevron-left"></i>','<i class="fal fa-chevron-right"></i>'],
+            responsive: {
+                  0: {
+                        items: 1
+                  },
+                  600: {
+                        items: 1
+                  },
+                  1000: {
+                        items: 1
+                  }
+            }
+      })
+      // Date
+      $("#datepicker").datepicker();
+});
+
