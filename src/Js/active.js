8c3b69422c365763f3a77cc27798edb7d2c2d302
shakibdshy <shakibdshy@gmail.com>
2018-12-22 01:45:56 +0600

    Why Choose Area Update

--- src/Js/active.js
+++ src/Js/active.js
@@ -43,5 +43,24 @@ $(document).ready(function () {
                   }
             }
       })
+
+      // Popular Courses Area
+      $('.why-choose-active').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: false,
+            dots: true,
+            responsive: {
+                  0: {
+                        items: 1
+                  },
+                  600: {
+                        items: 1
+                  },
+                  1000: {
+                        items: 3
+                  }
+            }
+      })
 });
 
