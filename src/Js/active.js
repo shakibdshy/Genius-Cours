1838b83e93a0a0f1eb9aaab09cd9691757bc1295
Md Habibur Rahman <shakibdshy@gmail.com>
2018-12-24 02:32:46 +0600

    Sponsor Area Update

--- src/Js/active.js
+++ src/Js/active.js
@@ -83,5 +83,25 @@ $(document).ready(function () {
             }
       });
 
+      // Brand Area
+      $('.brand-active').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: true,
+            dots: false,
+            navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+            responsive: {
+                  0: {
+                        items: 1
+                  },
+                  600: {
+                        items: 1
+                  },
+                  1000: {
+                        items: 6
+                  }
+            }
+      });
+
 });
 
