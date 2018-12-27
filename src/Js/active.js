969be26150562e30b06b92c60e7a4d39aa7daa4f
Md Habibur Rahman <shakibdshy@gmail.com>
2018-12-27 20:28:20 +0600

    Producta Area Update

--- src/Js/active.js
+++ src/Js/active.js
@@ -103,5 +103,25 @@ $(document).ready(function () {
             }
       });
 
+      // Product Area
+      $('.product-active').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: true,
+            dots: false,
+            navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+            responsive: {
+                  0: {
+                        items: 1
+                  },
+                  600: {
+                        items: 2
+                  },
+                  1000: {
+                        items: 4
+                  }
+            }
+      });
+
 });
 
